d65a55ffd4126ea4e0bd28d07a9b60bdaf34dfaf
JomarMullen <Jomar@pdx.edu>
2020-03-07 17:06:05 -0800

    GEOB-21 info panel was missing in last commit

--- src/view/MapView.js
+++ src/view/MapView.js
@@ -16,28 +16,38 @@ class MapView {
             ]
         }).fitWorld();
 
+        // set up the info control box
+        var myinfo = {lat: 0, lng: 0, zoom: this.map.getZoom()};
+        var info = L.control({position: 'bottomleft'});
+        info.onAdd = function (map) {
+            this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+            this.update();
+            return this._div;
+            
+        }
+        // updates the information displayed in the info control box
+        info.update = function(myinfo) {
+            this._div.innerHTML = (myinfo ? '<b> lat: ' + myinfo.lat + " long: " + myinfo.lng+ ' zoom: ' + myinfo.zoom + '</b>' 
+                    : 'unavailable');
+        }
+        info.addTo(this.map);
+
         // map event handlers
 
-        // gets lat lng on mouse move
+        // gets lat lng on mouse move and updates the info control box
         this.map.on("mousemove", function(event) {
-            var lat = event.latlng.lat;
-            var lng = event.latlng.lng;
-            console.log("lat :" + lat + " lng : " + lng);
+            var lat = event.latlng.lat.toFixed(2);
+            var lng = event.latlng.lng.toFixed(2);
+            myinfo.lat = lat;
+            myinfo.lng = lng;
+            info.update(myinfo);
 
         });
-        
-
-        //gets the zoom scale
-        var scale = this.map.getZoom()
-        console.log(scale);
-
+        // gets the zoom scale and updates the info control box
         this.map.on("zoomend", function(event){
-            // 
-            //console.log(event.getZoom());
-            //console.log(this.map.getZoom());
-            console.log(this.getZoom());
-            
-
+            var mapScale = this.getZoom();
+            myinfo.zoom = mapScale;
+            info.update(myinfo);
         });
 
     }
