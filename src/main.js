9b6e38c497cb795cd9153062122065330867a541
klhexpress <47010729+klhexpress@users.noreply.github.com>
2020-03-12 09:24:32 +0700

    GEOB-23 Button on top right to slide image pane

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 // Requires
-const {Controller} = require('./src/controller/Controller.js');
+const { Controller } = require('./src/controller/Controller.js');
 
 // Main entry point
-function main () {
+function main() {
     controller = new Controller('map');
 }
 
 main();
+
+function openNav() {
+    document.getElementById("AsideSection").style.width = "35%";
+    document.getElementById("main").style.marginRight = "35%";
+    document.getElementById("main").style.width = "65%";
+    document.getElementById("openAsideSection").style.visibility = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("AsideSection").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("main").style.width = "100%";
+    document.getElementById("openAsideSection").style.visibility = "visible";
+}
