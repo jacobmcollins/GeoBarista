1eb8158d72f6a06e3138f3b4c810d43b27a04c32
Jomar Mullen <Jomar@pdx.edu>
2020-04-06 13:14:54 -0700

    GEOB-26 removed unused code, unlinked customtoolbar from client, drawing gets geoJSON data , drawtoolbar is still missing icons

--- client/src/components/DrawTools.js
+++ client/src/components/DrawTools.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { EditControl} from 'react-leaflet-draw';
 import { FeatureGroup } from 'react-leaflet';
-import L, { bounds } from 'leaflet';
 
 
 // work around broken icons when using webpack, see https://github.com/PaulLeCam/react-leaflet/issues/255
@@ -13,15 +12,6 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-shadow.png',
 });
 
- const onEdited = (e) => {
-
-    let numEdited = 0;
-    e.layers.eachLayer( (layer) => {
-      numEdited += 1;
-    });
-    console.log(`_onEdited: edited ${numEdited} layers`, e);
-
-  }
   const onCreated = (e) => {
     let type = e.layerType;
     let layer = e.layer;
@@ -32,49 +22,13 @@ L.Icon.Default.mergeOptions({
     console.log("GeoJSON: ", test);
   }
 
-  const onDeleted = (e) => {
-
-    let numDeleted = 0;
-    e.layers.eachLayer( (layer) => {
-      numDeleted += 1;
-    });
-    console.log(`onDeleted: removed ${numDeleted} layers`, e);
-
-  }
-
-  const onMounted = (drawControl) => {
-    console.log('_onMounted', drawControl);
-  }
-
-  const onEditStart = (e) => {
-    console.log('_onEditStart', e);
-  }
-
-  const onEditStop = (e) => {
-    console.log('_onEditStop', e);
-  }
-
-  const onDeleteStart = (e) => {
-    console.log('_onDeleteStart', e);
-  }
-
-  const onDeleteStop = (e) => {
-    console.log('_onDeleteStop', e);
-  }
 
 export default function DrawTools(props) {
      return (
         <FeatureGroup fillColor="black" >
         <EditControl
              position = 'bottomright'
-             onEdited={onEdited}
              onCreated={onCreated}
-             onDeleted={onDeleted}
-             onMounted={onMounted}
-             onEditStart={onEditStart}
-             onEditStop={onEditStop}
-             onDeleteStart={onDeleteStart}
-             onDeleteStop={onDeleteStop}
              edit = {{
                edit : false,
                remove : true,
