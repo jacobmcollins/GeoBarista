01221d5a3d1b6b703885f4e669f6f25baad901c1
King-of-57 <henseltrevor@gmail.com>
2020-04-06 01:17:01 -0700

    GEOB-35 added initLocStorage.

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -2,11 +2,14 @@ import React from 'react'
 import {Drawer, Toolbar, IconButton} from '@material-ui/core';
 import {List, ListItem, ListItemText, ListItemIcon} from '@material-ui/core';
 import {Cancel as CancelIcon, Add as AddIcon} from '@material-ui/icons';
+import {initLocStorage} from "../Tools/initLocStorage";
+
 
 export default function MainMenu(props) {
     const {classes, toggleMainMenu, open, openDialog} = props;
     return (
-        <Drawer anchor="left" open={open} onClose={toggleMainMenu(false)}>
+        initLocStorage(true);
+    <Drawer anchor="left" open={open} onClose={toggleMainMenu(false)}>
             <div className={classes.mainMenu} role="presentation" onClick={toggleMainMenu(false)}>
                 <Toolbar className={classes.toolbar}>
                     <IconButton onClick={toggleMainMenu(false)}>
