a58defd4a91356824aaa28e0d67ac9d18fb0d0ee
jmc27 <jmc27@pdx.edu>
2020-04-08 11:41:39 -0700

    GEOB-29 enabled yarn start-electron

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { BrowserWindow, app } = require('electron');
-require('./server.js');
+server = require('./server.js');
 
 let mainWindow = null;
 
 function main() {
+  server('client/build')
   mainWindow = new BrowserWindow();
   mainWindow.loadURL(`http://localhost:3001`);
   mainWindow.on('close', event => {
