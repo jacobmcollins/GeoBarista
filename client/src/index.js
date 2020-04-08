77c5aee7e009b5408a7e797743ed8b8c1514734f
jmc27 <jmc27@pdx.edu>
2020-04-08 12:03:47 -0700

    GEOB-29 changed leaflet draw css import so that it does not affect screen size

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './Main';
+import '../node_modules/leaflet-draw/dist/leaflet.draw.css';
 
 ReactDOM.render(
   <Main />,
