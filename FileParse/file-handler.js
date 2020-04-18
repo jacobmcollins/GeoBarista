8172b18760bf10c20f21ed9ae4b31047bb897ad6
tim roach <timroach@pdx.edu>
2020-04-18 02:11:22 -0700

    GEOB-76 Added in ppjParse file to file-handler.js

--- FileParse/file-handler.js
+++ FileParse/file-handler.js
@@ -2,25 +2,26 @@ const {execSync} = require("child_process");
 var path = require('path');
 const imageService = require('../server/services/image');
 const imageModel = require('../server/models/image');
-
+var ppjParse = require('./ppjParse');
 
 class fileHandler {
     constructor(file_list) {        
-        this.file_list_obj = file_list;
+        this.file_list = file_list;
         this.processList();
         
     }
 
     async processList() {
-        console.log("json delivered: " + this.file_list_obj);
-        if(this.file_list_obj.length >= 1) {
-            for (const element of this.file_list_obj) {
+        console.log("json delivered: " + this.file_list);
+        if(this.file_list.length >= 1) {
+            for (const element of this.file_list) {
                 let fileext = path.extname(element.name);
                 switch (fileext) {
                     case ".ntf":
                         this.ntfinfo(element.path);
                         break;
                     case ".ppj":
+                        this.ppjinfo(element.path);
                         break;
                     case ".jpg":
                         break;
@@ -29,6 +30,12 @@ class fileHandler {
                 }
             }
         }
+    }
+
+    ppjinfo(path) {
+        var ppjParser = new ppjParse();
+        var metadata = ppjParser.convertXml(path);
+        metadata.logAllEntries();
     }
 
     async ntfinfo(path) {
@@ -46,4 +53,4 @@ class fileHandler {
     }
 }
 
-module.exports = fileHandler
+module.exports = fileHandler 
