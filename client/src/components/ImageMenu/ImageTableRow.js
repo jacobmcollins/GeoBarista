b83fbd21851f8778045a6401837b865f940004e4
jmc27 <jmc27@pdx.edu>
2020-04-27 19:51:46 -0700

    GEOB-100 removed all calls to image.file_path and image.file_ext for easier DB integration

--- client/src/components/ImageMenu/ImageTableRow.js
+++ client/src/components/ImageMenu/ImageTableRow.js
@@ -45,11 +45,9 @@ export default function ImageTableRow(props) {
             { 
                 columns.map((column) => {
                     return (
-                        <Tooltip title={image.file_path}>
-                            <TableCell component="th" scope="row" >
-                                {image[column.id]}
-                            </TableCell>
-                        </Tooltip>
+                        <TableCell component="th" scope="row" >
+                            {image[column.id]}
+                        </TableCell>
                     )
                 })
             }
